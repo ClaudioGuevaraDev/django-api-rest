531a28f99ea9b9a6c33819db9ccea061750fc7b5
ClaudioGuevaraDev <claudio.guevara.dev@gmail.com>
2022-08-19 21:39:11 -0400

    Intentando hacer middlewares

--- tasks/views.py
+++ tasks/views.py
@@ -1,19 +1,38 @@
+from urllib import request
 from rest_framework.decorators import api_view
 from django.http.response import JsonResponse
 from rest_framework import status
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+from django.utils.decorators import decorator_from_middleware
 
 from tasks.models import Task
 from tasks.serializers import TaskSerializer
 
+# def simple_middleware(get_response):
+#     def middleware(request):
+#         print("wenas")
+    
+#         return get_response(request)
+class SimpleMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+    
+    def __call__(self, request):
+        print("wenas")
+        return self.get_response(request)
+
+simple_decorator = decorator_from_middleware(SimpleMiddleware)
 
 @api_view(['GET', 'POST'])
+@simple_decorator
 def handler_tasks(request):
+    print(request.headers["Authorization"])
     if request.method == "GET":
         tasks = Task.objects.all()
         tasks_serializer = TaskSerializer(tasks, many=True)
-        return JsonResponse({"tasks": tasks_serializer.data}, status=status.HTTP_200_OK)
+        return Response({"tasks": tasks_serializer.data}, status=status.HTTP_200_OK)
     elif request.method == "POST":
         task_data = JSONParser().parse(request)
         task_serializer = TaskSerializer(data=task_data)
